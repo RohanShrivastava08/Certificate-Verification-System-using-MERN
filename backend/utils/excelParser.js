835331b0895e002341114a4941d0409c15b24256
Rohan Shrivastava <rohansh0808@gmail.com>
2024-09-25 12:00:47 +0530

    Add files via upload

---
+++ backend/utils/excelParser.js
@@ -0,0 +1,10 @@
+const xlsx = require('xlsx');
+
+const parseExcelFile = (filePath) => {
+  const workbook = xlsx.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const sheet = workbook.Sheets[sheetName];
+  return xlsx.utils.sheet_to_json(sheet);
+};
+
+module.exports = parseExcelFile;
